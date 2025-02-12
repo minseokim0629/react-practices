c3a42387dfa10e5b443343e6d84e5fe4960b5e29
minseokim0629 <han8121449@naver.com>
2025-02-13 00:12:53 +0900

    react #basics

---
+++ 01.Basics/ex05/src/app.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+    // const App = document.createElement('div');
+    // App.textContent = "Hello World";
+    // return App;
+    const App = React.createElement('div', null, 'Hello World'); // null - {id: '', title: ''} 작성하는 부분. 네번째 파라미터 부터 React.createElement('div', null, 'Hello World', React.createElement('div', null)); 이런식으로 작성 가능. 마치 재귀 형태..? 근데 이렇게 작성하면 너무 번거로워서 jsx?를 쓴다
+    return App;
+}
+
+export {App};
